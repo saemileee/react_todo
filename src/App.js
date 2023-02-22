3fb8fd41b28e70b47997af56de81a7e1f59d130a
saemileee <68241138+saemileee@users.noreply.github.com>
2023-02-23 03:12:53 +0900

    23-02-23 03:12 regular commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,69 @@ function App() {
   const [todoDB, setTodoDB] = useState([]);
   const [inputValue, setInputValue] = useState();
   const [selectedTags, setSelectedTags] = useState([]);
+  const [savedTagList, setSavedTagList] = useState([]);
+  const [newTag, setNewTag] = useState([]);
+  const [tagInputValue, setTagInputValue] = useState();
+  const [tagListStatus, setTagListStatus] = useState(false);
+
+  const showTagList = () => {
+    setTagListStatus((current) => !current);
+  };
+
+  const writeTagText = (e) => {
+    setTagInputValue(e.target.value);
+  };
+
+  const addNewTag = (e) => {
+    e.preventDefault();
+    setNewTag({ id: new Date().getTime(), value: tagInputValue });
+    const selectedTagFiltered = [...selectedTags].filter(
+      (tag) => tag.value == tagInputValue
+    );
+
+    if (selectedTagFiltered.length == 0) {
+      setSelectedTags([
+        ...selectedTags,
+        { id: new Date().getTime(), value: tagInputValue },
+      ]);
+    }
+    setTagInputValue("");
+
+    // 저장된 value 값이 이미 저장된 taglist의 value 값하고 같은 경우 찾았따!
+    //1. 작성한 태그가 selectedTag, savedTagList에 없는 경우
+    //2. 작성한 태그가 selectedTag에는 없고 savedTagList에 있는 경우
+    //3. 작성한 태그가 selectedTag에는 있고 savedTagList에 없는 경우
+    //4. 작성한 태그가 selectedTag, savedTagList 둘다 있는 경우
+  };
 
   const selectTag = (e) => {
-    setSelectedTags([
-      ...selectedTags,
-      { id: e.target.id, value: e.target.value },
-    ]);
+    if ([...selectedTags].filter((tag) => tag.id == e.target.id).length == 0) {
+      setSelectedTags([
+        ...selectedTags,
+        ...savedTagList.filter((tag) => tag.id == e.target.id),
+      ]);
+    }
+    //저장된 태그리스트에서 태그를 눌렀을 때 아이디가 같은 것을 셀렉티드로 추가
+    // + 중복되는 리스트는 추가 안되게 수정해야함
+  };
+
+  const updateTagList = () => {
+    // let newTags = [];
+    // for (let i = 0; i < savedTagList.length; i++) {
+    //   for (let j = 0; j < selectedTags.length; i++) {
+    //     if (savedTagList[i] !== selectedTags[j]) {
+    //       newTags.push(selectedTags[j]);
+    //     }
+    //   }
+    // }
+    setSavedTagList([...savedTagList, ...selectedTags]);
+    //투두리스트를 추가할 때 세이브드 태그 리스트에 없던 셀렉티드 태그 리스트를 세이브드에 추가하는 것
+  };
+
+  const delSelectedTag = (e) => {
+    setSelectedTags(
+      selectedTags.filter((tag) => e.target.parentElement.id != tag.id)
+    );
   };
 
   const writeTodoText = (e) => {
@@ -29,6 +86,7 @@ function App() {
     ]);
     setInputValue("");
     setSelectedTags([]);
+    updateTagList();
   };
 
   const completeCheck = (e) => {
@@ -50,18 +108,6 @@ function App() {
     <div>
       <h1>ToDo List</h1>
       <form onSubmit={addNewTodo}>
-        <select onChange={selectTag}>
-          <option>tags</option>
-          <option id="0" value="과제">
-            과제
-          </option>
-          <option id="1" value="약속">
-            약속
-          </option>
-          <option id="2" value="코딩">
-            코딩
-          </option>
-        </select>
         <input
           onChange={writeTodoText}
           value={inputValue}
@@ -71,12 +117,36 @@ function App() {
         />
         <button>추가</button>
       </form>
+      <form onSubmit={addNewTag}>
+        <input
+          onChange={writeTagText}
+          onFocus={showTagList}
+          onBlur={showTagList}
+          value={tagInputValue}
+          type="text"
+          placeholder="엔터로 태그 추가"
+        ></input>
+      </form>
+      <div style={{ display: tagListStatus ? "block" : "none" }}>
+        <p>태그 옵션 목록 (토글 필요)</p>
+        {savedTagList.map((tag) => (
+          <li id={tag.id} onClick={selectTag}>
+            {tag.value}
+          </li>
+        ))}
+      </div>
       <ul>
+        <span>선택한 태그: </span>
         {selectedTags.map((tag) => (
-          <span>#{tag.value} </span>
+          <span id={tag.id}>
+            #{tag.value}
+            <button onClick={delSelectedTag}>X</button>
+          </span>
         ))}
       </ul>
+      <hr />
       <ul>
+        <p>투두리스트</p>
         {todoDB.map((todoData) => (
           <li key={todoData.id} id={todoData.id}>
             <button onClick={completeCheck}>
@@ -86,7 +156,7 @@ function App() {
             <button onClick={deleteTodo}>X</button>
             <div>
               {todoData.tags.map((tag) => (
-                <span>{tag.value}</span>
+                <span>#{tag.value} </span>
               ))}
             </div>
           </li>
