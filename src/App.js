616b778f8526a804d045e2cf3b92ac33b08d3100
saemileee <68241138+saemileee@users.noreply.github.com>
2023-02-22 22:17:29 +0900

    23-02-22 22:17 regular commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [todoDB, setToddDB] = useState([]);
+  const [inputValue, setInputValue] = useState();
+  const writeTodoText = (e) => {
+    setInputValue(e.target.value);
+  };
+  const addNewTodo = (e) => {
+    e.preventDefault();
+    const newID = new Date().getTime();
+    setToddDB([
+      ...todoDB,
+      { id: newID, content: inputValue, isCompleted: false },
+    ]);
+    setInputValue("");
+  };
+  const completeCheck = (e) => {
+    todoDB.filter((data) =>
+      data.id == e.target.parentElement.id
+        ? data.isCompleted
+          ? (data.isCompleted = false)
+          : (data.isCompleted = true)
+        : null
+    );
+    console.log(todoDB);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo List</h1>
+      <form onSubmit={addNewTodo}>
+        <input
+          onChange={writeTodoText}
+          value={inputValue}
+          type="text"
+          placeholder="할 일을 입력해 주세요."
+        />
+        <button>추가</button>
+      </form>
+      <ul>
+        {todoDB.map((todoData) => (
+          <li id={todoData.id} data-iscompleted={todoData.isCompleted}>
+            <button onClick={completeCheck}>
+              {!todoData.isCompleted ? `🤔` : `😍`}
+            </button>
+            {todoData.content}
+            <button>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
